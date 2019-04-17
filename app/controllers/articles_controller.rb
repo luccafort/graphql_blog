af55cf9f4492e76a381670890446acb445e26dfc
luccafort <muspellsheim1985@gmail.com>
2019-04-17 11:06:25 +0900

    add new api endpoint.
    add to comments.
    add to create method for rest-ful api.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  # GET /articles GraphQLで置き換える前のREST-full API
   def index
     articles = Articles.recent
     render json: {
@@ -10,6 +11,20 @@ class ArticlesController < ApplicationController
           createdAt: article.created_at
         }
       end
+    }
+  end
+
+  # POST /articles GraphQLで置き換える前のREST-full API
+  def create
+    article = Article.new(params[:article].permit(:name, :content))
+    article.save!
+    render json: {
+      data: {
+        id: article.id,
+        name: article.name,
+        content: article.content,
+        createdAt: article.created_at,
+      },
     }
   end
 end
